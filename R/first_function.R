b6cb05b6698e06eb9c673f12d1591c199804645b
Nikola Surjanovic <16364942+nikola-sur@users.noreply.github.com>
2022-03-25 12:39:27 -0700

    Add link_canvas

--- R/first_function.R
+++
@@ -1,8 +0,0 @@
-#' First function
-#' Some description for users
-#'
-#' @return
-#' @export
-first_function <- function() {
-  print("Hello world!")
-}
---
+++ R/link_canvas.R
@@ -0,0 +1,11 @@
+#' Link to a Canvas course
+#' Establish a connection with the Canvas Instructure LMS API. Doing so allows for automatic updates to the Canvas course. This only needs to be performed once.
+#'
+#' @param token Canvas API token number. Can be obtained in Settings > Approved Integrations > New Access Token.
+#' @param course_id Canvas course number.
+#'
+#' @return
+#' @export
+link_canvas <- function(token, course_id) {
+  return(0)
+}
