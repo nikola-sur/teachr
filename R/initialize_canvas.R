c90c4d5ba4c7abb179673ed9a56ebdfb08c7fb45
Nikola Surjanovic <16364942+nikola-sur@users.noreply.github.com>
2022-04-01 10:13:06 -0700

    Add token functionality

--- R/initialize_canvas.R
+++ R/initialize_canvas.R
@@ -1,11 +1,13 @@
 #' Initialize Canvas settings for 'teachr' course
 #'
-#' Internal function. Creates a syllabus and home page for Canvas. Only run this
-#' function once.
+#' Creates a syllabus and home page for Canvas. Only run this function once.
 #'
 #' @param name See create_course()
 #' @param organization See create_course()
 #' @param path See create_course()
+#'
+#' @export
 initialize_canvas <- function(name, organization, path) {
+  # reticulate::py_run_string("x = 2")
   return(0)
 }
--- R/link_canvas.R
+++ R/link_canvas.R
@@ -2,10 +2,16 @@
 #'
 #' Establish a connection with the Canvas Instructure LMS API. Doing so allows for automatic updates to the Canvas course. This only needs to be performed once.
 #'
-#' @param token Canvas API token number. Can be obtained in Settings > Approved Integrations > New Access Token.
+#' @param token_path Path to where the Canvas API token number is stored. This
+#' should be stored in a file of type '.txt'. The Canvas token can be obtained in
+#' Settings > Approved Integrations > New Access Token.
 #' @param course_id Canvas course number.
 #'
 #' @export
-link_canvas <- function(token, course_id) {
-  return(0)
+link_canvas <- function(token_path, course_id) {
+  token <- as.character(utils::read.table(file = token_path))
+  utils::write.table(token_path, file = "token_path.txt")
+  # Update .gitignore so that the path is not visible? This is not a *major* concern
+  # but would be worth addressing.
+  # Do something with the token?
 }
