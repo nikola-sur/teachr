40ec100bbcaadf02f1658e38d5bded1f11d2b4be
Nikola Surjanovic <16364942+nikola-sur@users.noreply.github.com>
2022-04-08 17:40:03 -0700

    Update initialize_directories.R

--- R/initialize_directories.R
+++ R/initialize_directories.R
@@ -14,9 +14,9 @@ initialize_directories <- function(name, organization, path) {
   # Create organization folder
   dir.create(path = org_path, showWarnings = TRUE)
   usethis::create_project(path = org_path, open = FALSE)
-  old_wd <- getwd()
-  renv::init(project = paste0(organization, "/"))
-  setwd(old_wd)
+  # old_wd <- getwd()
+  # renv::init(project = paste0(organization, "/"))
+  # setwd(old_wd)
 
   # Create repo folders
   for (repo in repos) {
